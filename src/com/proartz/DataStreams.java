9dc0bf4bc80d8415e11c7e7193ac6195c1e86112
Marcin Filipowski <proartz.devon@gmail.com>
2019-02-14 15:51:56 +0100

    Add ObjectOutput testing

--- src/com/proartz/DataStreams.java
+++ src/com/proartz/DataStreams.java
@@ -1,11 +1,16 @@
 package com.proartz;
 
 import java.io.*;
+import java.math.BigDecimal;
 
 public class DataStreams {
-    public static final String dataName = "data.txt";
+    public static final String dataName = "data.dat";
 
-    public static final double[] prizes = {9.99, 10.23, 5.20, 6.40};
+    public static final BigDecimal[] prizes = {
+            new BigDecimal(9.99),
+            new BigDecimal(10.23),
+            new BigDecimal(5.20),
+            new BigDecimal(6.40)};
     public static final int[] units = {12, 8, 4, 5,};
     public static final String descriptions[] = {
             "Java T-Shirt",
@@ -13,14 +18,14 @@ public class DataStreams {
             "Java Book",
             "Java Development Kit"};
 
-    public static void main(String[] args) throws IOException{
-//        DataOutputStream output = null;
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
+//        ObjectOutputStream output = null;
 //        try {
-//            output = new DataOutputStream(new BufferedOutputStream(
+//            output = new ObjectOutputStream(new BufferedOutputStream(
 //                        new FileOutputStream(dataName)));
 //
 //            for(int i = 0; i < prizes.length; i++) {
-//                output.writeDouble(prizes[i]);
+//                output.writeObject(prizes[i]);
 //                output.writeInt(units[i]);
 //                output.writeUTF(descriptions[i]);
 //            }
@@ -30,27 +35,29 @@ public class DataStreams {
 //            }
 //        }
 
-        DataInputStream input = null;
-
-        double prize;
-        int unit;
-        String description;
-        double total = 0;
-
-        try{
-            input = new DataInputStream(new BufferedInputStream(new FileInputStream(dataName)));
-
-            while(true) {
-                prize = input.readDouble();
-                unit = input.readInt();
-                description = input.readUTF();
-                System.out.format("You bought %d units of %s for %.2f.%n", unit, description, prize);
-
-                total += prize * unit;
-            }
-        } catch(EOFException e) {
-
-        }
-        System.out.format("Total: %.2f", total);
+//        DataInputStream input = null;
+//
+//        double prize;
+//        int unit;
+//        String description;
+//        double total = 0;
+//
+//        try{
+//            input = new DataInputStream(new BufferedInputStream(
+//                        new FileInputStream(dataName)));
+//
+//            while(true) {
+//                prize = input.readDouble();
+//                unit = input.readInt();
+//                description = input.readUTF();
+//                System.out.format("You bought %d units of %s for %.2f.%n",
+//                        unit, description, prize);
+//
+//                total += prize * unit;
+//            }
+//        } catch(EOFException e) {
+//
+//        }
+//        System.out.format("Total: %.2f", total);
     }
 }
